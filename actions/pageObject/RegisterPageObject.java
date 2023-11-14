a91cd9a6c9c0e79dd456134edf85d332d048ff1a
PhuongNguyenThiHong <nphuong184@gmail.com>
2023-11-14 09:34:07 +0700

    Page Object 1

--- actions/pageObject/RegisterPageObject.java
+++ actions/pageObject/RegisterPageObject.java
@@ -1,5 +1,89 @@
 package pageObject;
 
-public class RegisterPageObject {
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.RegisterPageUI;
+
+public class RegisterPageObject extends BasePage{
+
+    WebDriver driver;
+   
+	public RegisterPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void clickToRegisterButton() {
+		
+		waitForElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
+		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
+		
+	}
+	
+	public String getErrorMessageAtFirstnameTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.FIRSTNAME_ERROR_MESSAGE);
+		return getElementText(driver, RegisterPageUI.FIRSTNAME_ERROR_MESSAGE);
+	}
+
+	public String getErrorMessageAtLastnameTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.LASTNAME_ERROR_MESSAGE);
+		return getElementText(driver, RegisterPageUI.LASTNAME_ERROR_MESSAGE);
+	}
+
+	public String getErrorMessageAtEmailTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_ERROR_MESSAGE);
+		return getElementText(driver, RegisterPageUI.EMAIL_ERROR_MESSAGE);
+	}
+
+	public String getErrorMessageAtPasswordTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.PASSWORD_ERROR_MESSAGE);
+		return getElementText(driver, RegisterPageUI.PASSWORD_ERROR_MESSAGE);
+	}
+
+	public String getErrorMessageAtConfirmPasswordTextbox() {
+		waitForElementVisible(driver, RegisterPageUI.CONFIRMPASSWORD_ERROR_MESSAGE);
+		return getElementText(driver, RegisterPageUI.CONFIRMPASSWORD_ERROR_MESSAGE);
+	}
+
+	public void inputToFirstNameTextbox(String firstName) {
+		waitForElementVisible(driver, RegisterPageUI.INPUT_FIRSTNAME);
+		sendKeyToElement(driver, RegisterPageUI.INPUT_FIRSTNAME, firstName);
+		
+	}
+
+	public void inputToLastNameTextbox(String lastName) {
+		waitForElementVisible(driver, RegisterPageUI.INPUT_LASTNAME);
+		sendKeyToElement(driver, RegisterPageUI.INPUT_LASTNAME, lastName);
+		
+	}
+
+	public void inputToEmailTextbox(String email) {
+		waitForElementVisible(driver, RegisterPageUI.INPUT_EMAIL);
+		sendKeyToElement(driver, RegisterPageUI.INPUT_EMAIL, email);
+		
+	}
+
+	public void inputToPasswordTextbox(String password) {
+		waitForElementVisible(driver, RegisterPageUI.INPUT_PASSWORD);
+		sendKeyToElement(driver, RegisterPageUI.INPUT_PASSWORD, password);
+		
+	}
+
+	public void inputToConfirmPasswordTextbox(String confirmpassword) {
+		waitForElementVisible(driver, RegisterPageUI.INPUT_CONFIRMPASSWORD);
+		sendKeyToElement(driver, RegisterPageUI.INPUT_CONFIRMPASSWORD, confirmpassword);
+		
+	}
+
+	public String getRegisterSuccessMessage() {
+		waitForElementVisible(driver, RegisterPageUI.REGISTER_SUCCESS_MESSAGE);
+		return getElementText(driver, RegisterPageUI.REGISTER_SUCCESS_MESSAGE);
+	}
+
+	public String getExistEmailMessage() {
+		waitForElementVisible(driver, RegisterPageUI.EXIST_EMAIL_MESSAGE);
+		return getElementText(driver, RegisterPageUI.EXIST_EMAIL_MESSAGE);
+	}
+
 
 }
--- interfaces/pageUIs/RegisterPageUI.java
+++ interfaces/pageUIs/RegisterPageUI.java
@@ -1,6 +1,21 @@
 package pageUIs;
 
 public class RegisterPageUI {
-	public static final String EMAIL_TEXTBOX="//@id='Email'";
+	public static final String REGISTER_BUTTON="//button[@id='register-button']";
+	public static final String FIRSTNAME_ERROR_MESSAGE="//span[@id='FirstName-error']";
+	public static final String LASTNAME_ERROR_MESSAGE="//span[@id='LastName-error']";
+	public static final String EMAIL_ERROR_MESSAGE="//span[@id='Email-error']";
+	public static final String PASSWORD_ERROR_MESSAGE="//span[@id='Password-error']";
+	public static final String CONFIRMPASSWORD_ERROR_MESSAGE="//span[@id='ConfirmPassword-error']";
+	
+	public static final String INPUT_FIRSTNAME="//input[@id='FirstName']";
+	public static final String INPUT_LASTNAME="//input[@id='LastName']";
+	public static final String INPUT_EMAIL="//input[@id='Email']";
+	public static final String INPUT_PASSWORD="//input[@id='Password']";
+	public static final String INPUT_CONFIRMPASSWORD="//input[@id='ConfirmPassword']";
+	
+	public static final String REGISTER_SUCCESS_MESSAGE="//div[@class='result']";
+	public static final String EXIST_EMAIL_MESSAGE="//div[contains(@class,'message-error')]//li";
+	
 
 }
